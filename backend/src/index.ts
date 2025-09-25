ec608a4cb9f1679088e56ba5107bb1bb18001098
thatsninou <nourkhelfallah3@gmail.com>
2025-09-25 12:20:41 +0100

    cors problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://127.0.0.1:5500", // or http://localhost:5500 if using Live Server
+        origin: "http://localhost:5173", // or http://localhost:5500 if using Live Server
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
